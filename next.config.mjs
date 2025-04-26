6a1d8492e656ec5ad2677f2fd1b51a1573029216
amaury.pichat@gmail.com <amaury.pichat@gmail.com>
2025-04-26 16:40:52 +0200

    avant test ecolo 2

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,51 @@ const nextConfig = {
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
-  }
+  },
+  async headers() {
+    return [
+      // Images
+      {
+        source: '/(.*)\\.(jpg|jpeg|gif|png|svg|ico|webp)$',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      // Fonts
+      {
+        source: '/(.*)\\.(woff|woff2|ttf|otf|eot)$',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      // CSS et JS
+      {
+        source: '/(.*)\\.(css|js)$',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      // Pages HTML
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
